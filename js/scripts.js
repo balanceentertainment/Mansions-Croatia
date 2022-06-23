36b2264386010e4dfda93647e64a9c1bd780b32f
DjonDark <mrak.ivan@gmail.com>
2022-06-23 13:58:00 +0200

    removed animated cursor

--- js/scripts.js
+++ js/scripts.js
@@ -415,130 +415,130 @@
   var swiper = new Swiper(".main-slider", swiperOptions);
 
   /* MAGNET CURSOR*/
-  var cerchio = document.querySelectorAll('.hamburger-menu');
-  cerchio.forEach(function (elem) {
-    $(document).on('mousemove touch', function (e) {
-      magnetize(elem, e);
-    });
-  })
+  // var cerchio = document.querySelectorAll('.hamburger-menu');
+  // cerchio.forEach(function (elem) {
+  //   $(document).on('mousemove touch', function (e) {
+  //     magnetize(elem, e);
+  //   });
+  // })
 
-  function magnetize(el, e) {
-    var mX = e.pageX,
-      mY = e.pageY;
-    const item = $(el);
+  // function magnetize(el, e) {
+  //   var mX = e.pageX,
+  //     mY = e.pageY;
+  //   const item = $(el);
 
-    const customDist = item.data('dist') * 20 || 80;
-    const centerX = item.offset().left + (item.width() / 2);
-    const centerY = item.offset().top + (item.height() / 2);
+  //   const customDist = item.data('dist') * 20 || 80;
+  //   const centerX = item.offset().left + (item.width() / 2);
+  //   const centerY = item.offset().top + (item.height() / 2);
 
-    var deltaX = Math.floor((centerX - mX)) * -0.35;
-    var deltaY = Math.floor((centerY - mY)) * -0.35;
+  //   var deltaX = Math.floor((centerX - mX)) * -0.35;
+  //   var deltaY = Math.floor((centerY - mY)) * -0.35;
 
-    var distance = calculateDistance(item, mX, mY);
+  //   var distance = calculateDistance(item, mX, mY);
 
-    if (distance < customDist) {
-      TweenMax.to(item, 0.5, {
-        y: deltaY,
-        x: deltaX,
-        scale: 1
-      });
-      item.addClass('magnet');
-    } else {
-      TweenMax.to(item, 0.6, {
-        y: 0,
-        x: 0,
-        scale: 1
-      });
-      item.removeClass('magnet');
-    }
-  }
+  //   if (distance < customDist) {
+  //     TweenMax.to(item, 0.5, {
+  //       y: deltaY,
+  //       x: deltaX,
+  //       scale: 1
+  //     });
+  //     item.addClass('magnet');
+  //   } else {
+  //     TweenMax.to(item, 0.6, {
+  //       y: 0,
+  //       x: 0,
+  //       scale: 1
+  //     });
+  //     item.removeClass('magnet');
+  //   }
+  // }
 
-  function calculateDistance(elem, mouseX, mouseY) {
-    return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left + (elem.width() / 2)), 2) + Math.pow(mouseY - (elem.offset().top + (elem.height() / 2)), 2)));
-  }
+  // function calculateDistance(elem, mouseX, mouseY) {
+  //   return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left + (elem.width() / 2)), 2) + Math.pow(mouseY - (elem.offset().top + (elem.height() / 2)), 2)));
+  // }
 
-  function lerp(a, b, n) {
-    return (1 - n) * a + n * b
-  }
+  // function lerp(a, b, n) {
+  //   return (1 - n) * a + n * b
+  // }
 
   // Inizio Cursor
-  class Cursor {
-    constructor() {
-      this.bind()
-      //seleziono la classe del cursore
-      this.cursor = document.querySelector('.js-cursor')
+  // class Cursor {
+  //   constructor() {
+  //     this.bind()
+  //     //seleziono la classe del cursore
+  //     this.cursor = document.querySelector('.js-cursor')
 
-      this.mouseCurrent = {
-        x: 0,
-        y: 0
-      }
+  //     this.mouseCurrent = {
+  //       x: 0,
+  //       y: 0
+  //     }
 
-      this.mouseLast = {
-        x: this.mouseCurrent.x,
-        y: this.mouseCurrent.y
-      }
+  //     this.mouseLast = {
+  //       x: this.mouseCurrent.x,
+  //       y: this.mouseCurrent.y
+  //     }
 
-      this.rAF = undefined
-    }
+  //     this.rAF = undefined
+  //   }
 
-    bind() {
-      ['getMousePosition', 'run'].forEach((fn) => this[fn] = this[fn].bind(this))
-    }
+  //   bind() {
+  //     ['getMousePosition', 'run'].forEach((fn) => this[fn] = this[fn].bind(this))
+  //   }
 
-    getMousePosition(e) {
-      this.mouseCurrent = {
-        x: e.clientX,
-        y: e.clientY
-      }
-    }
+  //   getMousePosition(e) {
+  //     this.mouseCurrent = {
+  //       x: e.clientX,
+  //       y: e.clientY
+  //     }
+  //   }
 
-    run() {
-      this.mouseLast.x = lerp(this.mouseLast.x, this.mouseCurrent.x, 0.2)
-      this.mouseLast.y = lerp(this.mouseLast.y, this.mouseCurrent.y, 0.2)
+  //   run() {
+  //     this.mouseLast.x = lerp(this.mouseLast.x, this.mouseCurrent.x, 0.2)
+  //     this.mouseLast.y = lerp(this.mouseLast.y, this.mouseCurrent.y, 0.2)
 
-      this.mouseLast.x = Math.floor(this.mouseLast.x * 100) / 100
-      this.mouseLast.y = Math.floor(this.mouseLast.y * 100) / 100
+  //     this.mouseLast.x = Math.floor(this.mouseLast.x * 100) / 100
+  //     this.mouseLast.y = Math.floor(this.mouseLast.y * 100) / 100
 
-      this.cursor.style.transform = `translate3d(${this.mouseLast.x}px, ${this.mouseLast.y}px, 0)`
+  //     this.cursor.style.transform = `translate3d(${this.mouseLast.x}px, ${this.mouseLast.y}px, 0)`
 
-      this.rAF = requestAnimationFrame(this.run)
-    }
+  //     this.rAF = requestAnimationFrame(this.run)
+  //   }
 
-    requestAnimationFrame() {
-      this.rAF = requestAnimationFrame(this.run)
-    }
+  //   requestAnimationFrame() {
+  //     this.rAF = requestAnimationFrame(this.run)
+  //   }
 
-    addEvents() {
-      window.addEventListener('mousemove', this.getMousePosition, false)
-    }
+  //   addEvents() {
+  //     window.addEventListener('mousemove', this.getMousePosition, false)
+  //   }
 
-    on() {
-      this.addEvents()
+  //   on() {
+  //     this.addEvents()
 
-      this.requestAnimationFrame()
-    }
+  //     this.requestAnimationFrame()
+  //   }
 
-    init() {
-      this.on()
-    }
-  }
+  //   init() {
+  //     this.on()
+  //   }
+  // }
 
-  if ($('.js-cursor').length > 0) {
-    const cursor = new Cursor()
+  // if ($('.js-cursor').length > 0) {
+  //   const cursor = new Cursor()
 
-    cursor.init();
+  //   cursor.init();
 
 
-    /* CURSOR CONDITIONS */
-    $('.carousel-image-box').hover(function () {
-      $('.cursor').toggleClass('drag');
-    });
+  //   /* CURSOR CONDITIONS */
+  //   $('.carousel-image-box').hover(function () {
+  //     $('.cursor').toggleClass('drag');
+  //   });
 
-    $('a, .hamburger-menu, .equalizer, .swiper-pagination-bullet, .swiper-button-prev, .swiper-button-next, .main-nav button, submit[type="submit"], .rooms-slider .room-detail h3').hover(function () {
-      $('.cursor').toggleClass('light');
-    });
+  //   $('a, .hamburger-menu, .equalizer, .swiper-pagination-bullet, .swiper-button-prev, .swiper-button-next, .main-nav button, submit[type="submit"], .rooms-slider .room-detail h3').hover(function () {
+  //     $('.cursor').toggleClass('light');
+  //   });
 
-  }
+  // }
 
 
   // PRELOADER
